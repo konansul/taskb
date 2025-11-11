5c944537f9afd1a4cebbadacd9cdcabd267533ca
Kanan Sultanov <konansulx@gmail.com>
2025-11-12 00:16:46 +0400

    Adding .gitignore

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/konansul/Desktop/4sim-gov-ai-mvp/taskb/venv"
+set -gx VIRTUAL_ENV "/Users/konansul/taskb/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
